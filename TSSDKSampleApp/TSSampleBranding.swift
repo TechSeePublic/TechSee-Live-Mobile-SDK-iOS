e154883ad0c1f61e94bc62a4f3c1bdc086ee2412
Hanoch Mordkovich <hanoch.mordkovich@techsee.me>
2025-02-18 17:01:22 +0200

    finish setup

---
+++ TSSDKSampleApp/TSSampleBranding.swift
@@ -0,0 +1,12 @@
+//
+//  TSSampleBranding.swift
+//  TSSDKSampleApp
+//
+//  Created by Hanoch Mordkovich on 13/12/2023.
+//
+
+import Foundation
+import TechSeeLiveFramework
+
+class VodafoneBranding: TSBranding {
+}
